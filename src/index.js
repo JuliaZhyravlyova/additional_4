1d482478f2015ed2c4387f2aee30b252836b2b6b
JuliaZhyravlyova <31968139+JuliaZhyravlyova@users.noreply.github.com>
2017-09-29 23:48:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function multiply(first, second) {
   // your solution
+  
+  
+  
+      
+
+  const shortNumsResults = first.split('').reverse().map(
+    (firstValue) => second.split('').reverse().map((secondValue) => firstValue * secondValue)
+  );
+
+  
+  const flatResults = shortNumsResults.reduce((acc, arr, startPosition) => {
+    arr.map((value, index) => {
+      const radix = index + startPosition;
+      acc[radix] = (acc[radix] || 0) + value;
+    })
+    return acc;
+  }, [])
+
+
+  while(!!flatResults.find((number) => number > 9)) {
+    flatResults.map((number, index) => {
+      if (number > 9) {
+        flatResults[index] = number % 10;
+        flatResults[index + 1] = (flatResults[index + 1] || 0) + Math.floor(number / 10);
+      }
+    })
+  }
+
+  return flatResults.reverse().join('');
 }
